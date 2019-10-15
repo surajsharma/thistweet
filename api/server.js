eb4f57d6eb5089a04378570be627187e364f914f
suraj <surajsharma@pm.me>
2019-10-15 22:25:00 +0530

    text chop fix

--- api/server.js
+++ api/server.js
@@ -18,18 +18,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 function makeItSquare(imageB64) {
     var filepath = base64Img.imgSync(imageB64, tmp, "tmp");
+
     var dimensions = sizeOf(filepath);
+
     console.log("Image Dim:(before) " + JSON.stringify(dimensions));
+
     var squareDimension = Math.max(dimensions.width, dimensions.height) + 100;
+
     console.log("Square dim: " + squareDimension);
 
-    var canvas = new Canvas(squareDimension, squareDimension);
+    var canvas = new Canvas.createCanvas(squareDimension, squareDimension);
 
     var img = new Canvas.Image();
     img.src = imageB64;
 
     var context = canvas.getContext("2d");
-
     context.strokeStyle = "black";
     context.lineWidth = 60;
     context.strokeRect(0, 0, squareDimension, squareDimension);
@@ -40,14 +43,14 @@ function makeItSquare(imageB64) {
     // top text
 
     context.fillStyle = "white";
-    context.font = "20px Helvetica";
-    context.fillText("@_abd_ur_rahman", 30, 20);
+    context.font = "18px Helvetica";
 
     context.drawImage(
         img,
         (squareDimension - dimensions.width) / 2,
         squareDimension / 2 - dimensions.height / 2
     );
+
     return canvas.toDataURL();
 }
 
@@ -80,55 +83,7 @@ app.get("/image/:text", function(req, res) {
     var options = {
         debug: true,
         maxWidth: 400,
-        fontSize: 30,
-        margin: 20,
-        bgColor: "rgba(0, 0, 0, 0.95)",
-        textColor: "white"
-    };
-
-    textToImage
-        .generate(text, options)
-        .then((dataUrl) => {
-            var finalImage = makeItSquare(dataUrl);
-            if (isRaw) {
-                res.end(finalImage.substring(22), "base64");
-            } else {
-                res.status(200).send(
-                    `<a href="${finalImage}" download="${fileName}"><img title="${fileName}" src="${finalImage}" /></a>`
-                );
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.post("/image", function(req, res) {
-    var username = req.body.credit ? req.body.credit : null;
-
-    var isRaw = req.query.raw == 1;
-
-    var text = req.body.text;
-
-    const fileName =
-        text
-            .substring(0, 15)
-            .toLowerCase()
-            .split(" ")
-            .join("_")
-            .split(",")
-            .join("")
-            .split(".")
-            .join("") + ".png";
-
-    console.log(fileName);
-
-    if (username) text += "\n-" + username;
-
-    var options = {
-        debug: true,
-        maxWidth: 400,
-        fontSize: 30,
+        fontSize: 28,
         margin: 20,
         bgColor: "rgba(0, 0, 0, 0.95)",
         textColor: "white"
