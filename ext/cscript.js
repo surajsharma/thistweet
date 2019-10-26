dac948e33194b884a4f05913bb53f1a499d22fce
suraj <surajsharma@pm.me>
2019-10-26 23:25:14 +0530

    removed selection sharer, jQuery, jQueryUI

--- ext/cscript.js
+++ ext/cscript.js
@@ -1,10 +1,7 @@
-document.addEventListener("mouseup", function(event) {
+window.addEventListener("mouseup", function(event) {
     var sel = window.getSelection();
-
-    // var range = sel.getRangeAt(0);
-    // rect = range.getBoundingClientRect();
-
     if (sel.toString().length) {
-        $(document).selectionSharer();
+        // $(document).selectionSharer();
+        alert(sel);
     }
 });
