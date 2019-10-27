83a0cdc704cfd9111086a2b98ff3606831cc17cb
suraj <surajsharma@pm.me>
2019-10-27 14:51:33 +0530

    selection to image working

--- ext/cscript.js
+++ ext/cscript.js
@@ -2,7 +2,9 @@ window.addEventListener("mouseup", function(event) {
     var sel = window.getSelection();
     if (sel.toString().length) {
         // $(document).selectionSharer();
-        console.log(sel.toString());
-        window.open("http://localhost:3101/image/" + sel + "/ok");
+        console.log(sel.toString(), window.location.href);
+        window.open(
+            "http://localhost:3101/image/" + sel + "/" + window.location.href
+        );
     }
 });
