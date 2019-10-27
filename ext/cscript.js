1db34b305a2ae750883b7534d10c0387af0aa9b1
suraj <surajsharma@pm.me>
2019-10-27 14:30:19 +0530

    added window.open on mouseup listener

--- ext/cscript.js
+++ ext/cscript.js
@@ -2,6 +2,7 @@ window.addEventListener("mouseup", function(event) {
     var sel = window.getSelection();
     if (sel.toString().length) {
         // $(document).selectionSharer();
-        alert(sel);
+        console.log(sel.toString());
+        window.open("http://localhost:3101/image/" + sel + "/ok");
     }
 });
