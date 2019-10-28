2959324c91e9a58d264f474613357f7521450f7a
suraj <surajsharma@pm.me>
2019-10-28 20:57:47 +0530

    implemente max length

--- ext/cscript.js
+++ ext/cscript.js
@@ -1,6 +1,6 @@
 window.addEventListener("mouseup", function(event) {
     var sel = window.getSelection();
-    if (sel.toString().length) {
+    if (sel.toString().length && sel.toString().length <= 1000) {
         console.log(sel.toString(), window.location.href);
         window.open(
             "http://localhost:3101/image?&text=" +
