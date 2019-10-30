f168f7d7785fc3d578b73a107d56fa2d888481c5
suraj <surajsharma@pm.me>
2019-10-30 08:20:43 +0530

    update gitignore

--- ext/cscript.js
+++ ext/cscript.js
@@ -1,12 +1,15 @@
-window.addEventListener("mouseup", function(event) {
-    var sel = window.getSelection();
-    if (sel.toString().length && sel.toString().length <= 1000) {
-        console.log(sel.toString(), window.location.href);
-        window.open(
-            "http://localhost:3101/image?&text=" +
-                sel +
-                "&source=" +
-                window.location.href
-        );
-    }
+window.addEventListener('mouseup', function(event) {
+  var sel = window.getSelection();
+  if (
+    sel.toString().length <= 1000 &&
+    sel.toString().length > 100
+  ) {
+    console.log(sel.toString(), window.location.href);
+    window.open(
+      'http://localhost:3101/image?&text=' +
+        encodeURIComponent(sel) +
+        '&source=' +
+        encodeURIComponent(window.location.href),
+    );
+  }
 });
